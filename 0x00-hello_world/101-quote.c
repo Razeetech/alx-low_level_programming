01158218210f58ed643a7f1ef90d0f15d58b50c8
Razeetech <rajiazeezsch@gmail.com>
2022-12-08 20:13:37 -0800

    C-programe

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
+ * main - main exactly and that piece of art is useful
  *
- * main - prints exactly and that piece of art is useful
- *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
-
 int main(void)
-
 {
-	 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	 return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-1-0-19\n", 59);
+	return (1);
 }
