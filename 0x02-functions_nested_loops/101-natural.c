c969d7039d9c2f6ac2f6d1d12dabf9e142becfb3
Razeetech <rajiazeezsch@gmail.com>
2022-12-13 19:28:24 -0800

    c function 2

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  * main -main block
- * description: computes and prints the sum of all the multiples of 3 or 
+ * description: computes and prints the sum of all the multiples of 3 or
  * 5 below 1024 (excluded), followed by a new line
  * Return: 0
  */
