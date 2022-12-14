8467a8e7de5e86938e92d5d3212cd267f3210ac8
Razeetech <rajiazeezsch@gmail.com>
2022-12-14 03:33:03 -0800

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main main block
- * Description: compute and prints sum of all multiples of 5 below 1024
+ * main - main block
+ * Description: compute and prints sum of all multiples of 3 or
+ * 5 below 1024 (excluded), followed by a new line
  * Return: 0
  */
 int main(void)
@@ -9,7 +10,7 @@ int main(void)
 	unsigned long int i;
 	unsigned long int bef = 1;
 	unsigned long int aft = 2;
-	unsigned long int i = 1000000000;
+	unsigned long int l = 1000000000;
 	unsigned long int bef1;
 	unsigned long int bef2;
 	unsigned long int aft1;
@@ -24,15 +25,15 @@ int main(void)
 		bef = aft - bef;
 	}
 
-	bef1 = (bef /1);
-	bef2 = (bef % 1);
-	aft1 = (aft / 1);
-	aft2 = (aft % 1);
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
 
-	for (i =92; i < 99; ++i)
+	for (i = 92; i < 99; ++i)
 	{
-		printf(", %lu", aft1 +(aft2 / 1));
-		printf("%lu", aft2 % 1);
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
 		aft1 = aft1 + bef1;
 		bef1 = aft1 - bef1;
 		aft2 = aft2 + bef2;
