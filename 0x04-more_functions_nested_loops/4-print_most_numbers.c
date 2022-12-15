a5312e955cff22e7b38c1164c50d555459c073c8
Razeetech <rajiazeezsch@gmail.com>
2022-12-15 14:06:37 -0800

    multiply input

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,17 +3,19 @@
 /**
  * print_most_numbers - prints numbers between 0 and 9
  * 2 and 4 not included
- * Return: no return
+ * Return: 0-9
  */
 
 void print_most_numbers(void)
 {
 	int ch;
 
-	for (ch = 48; ch < 58; ch++)
+	for (ch = 0; ch <= 9; ch++)
 	{
-		if (ch != 50 && ch != 52)
-			_putchar(ch);
+		if ((ch == 2) || (ch == 4))
+			continue;
+		else
+			_putchar(i + '0');
 	}
 	_putchar('\n');
 }
