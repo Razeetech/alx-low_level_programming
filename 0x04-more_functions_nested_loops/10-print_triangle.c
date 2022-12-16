f73cbc9f40748dba6ef5ff21868eea9be599695e
Razeetech <rajiazeezsch@gmail.com>
2022-12-16 03:10:53 -0800

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,17 +2,16 @@
 
 /**
  * print_triangle - prints a triangle
- * @size: numbers of lines
+ * @size: number of size
  * Return: no return
  */
-
 void print_triangle(int size);
 {
 	int i, j;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < (size - i); j++)
+		for (j = 1; j < (size - i); j++)
 			_putchar(' ');
 		for (j--; j < size; j++)
 			_putchar(35);
