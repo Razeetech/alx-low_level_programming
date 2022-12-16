f50441ae8be6308684452d969631b171668f759d
Razeetech <rajiazeezsch@gmail.com>
2022-12-16 02:03:07 -0800

    take 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,12 +12,12 @@ void print_triangle(int size);
 
 	for (i = 0; i < size; i++)
 	{
-	for (i = 0; i < (size - i); j++)
-		_putchar(' ');
-	for (j--; j < size; j++)
-		_putchar(35);
-	if (i < (size - 1))
-		_putchar('\n');
+		for (j = 0; j < (size - i); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar(35);
+		if (i < (size - 1))
+			_putchar('\n');
 	}
 	_putchar('\n');
 }
