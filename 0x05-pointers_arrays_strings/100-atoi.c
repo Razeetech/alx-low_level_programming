9d07ac049341c271a60b885aecf0f934f739b1d2
Razeetech <rajiazeezsch@gmail.com>
2022-12-20 11:23:48 -0800

    C pointers and arrays

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,21 +1,33 @@
 #include "main.h"
 
 /**
- * _strcpy - copies the string printerd to by src
- * @dest: destination
- * @src: source
- * Return: string
+ * _atoi - converts a string to an inter.
+ * @s: input string.
+ * Return: interger.
  */
-char *_strcpy(char *dest, char *src)
+int _atoi(char *s)
 {
-	int len = 0;
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-	while (*(src + len) != '\0')
+	while (*(s + count) != '\0')
 	{
-		*(dest + len) = *(src + len);
-		len++;
-	}
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+		if (*(s + count) == '-')
+			pn *= -1;
 
-	*(dest + len) = '\0';
-	return (dest);
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++
+		}
+		count++;
+	}
+	for (i = count - size; i < count; i++)
+	{
+		oi =oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
 }
