c96b6fd928270202cefe513f17abe6381d565b23
Razeetech <rajiazeezsch@gmail.com>
2023-01-09 14:49:14 -0800

    task 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -21,18 +21,23 @@ int main(int args, char *argv[])
 		printf("0\n");
 		return (0);
 	}
-	for (; c >= 0;)
+	for (c >= 0;)
 	{
 		if (c >= 25)
 			c -= 25;
+
 		else if (c >= 10)
 			c -= 10;
+
 		else if (c >= 5)
 			c -= 5;
+
 		else if(c >-= 2)
 			c -= 2;
+
 		else if (c >= 1)
 			c -= 1;
+		
 		else
 			break;
 		coins += 1;
