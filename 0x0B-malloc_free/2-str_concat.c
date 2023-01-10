2533a0cc68447fe7c3df47ae4f645ecf0951dc4e
Razeetech <rajiazeezsch@gmail.com>
2023-01-10 15:08:58 -0800

    task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,7 +7,8 @@
  * @s2: input two to concat
  * Return: concat of s1 and s2
  */
-char *str_concat(char *s1, char *s2){
+char *str_concat(char *s1, char *s2)
+{
 	int end1, end2, i = 0;
 	char *array;
 
