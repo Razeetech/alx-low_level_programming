0b7f3b97ca30c5a1a7ce18a777396c0e9f5b0556
Razeetech <rajiazeezsch@gmail.com>
2023-01-10 20:01:32 -0800

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,33 +8,39 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **iarray;
-	int i, n;
+	int i, j, a, b;
+	int **p;
 
 	if (width <= 0 || height <= 0)
+	{
 		return (NULL);
-
-	iarray = malloc(sizeof(int) * height);
-
-	if (iarray == NULL)
-	{
-		free(iarray);
-		return(NULL);
 	}
-
-	for (i = 0; i < height; i++)
+	else
 	{
-		iarray[i] = malloc(sizeof(int) * width);
-		if (iarray == NULL)
+		p = (int **) malloc(height * sizeof(int *));
+		if (!p)
 		{
-			free(iarray);
+			free(p);
 			return (NULL);
 		}
-
-		for (n = 0; n < width; n++)
+		for (i = 0; i < height; i++)
 		{
-			iarray[i][n] = 0;
+			p[i] = (int *) malloc(width * sizeof(int));
+			if(!p[i])
+			{
+				for (j = 0; j <= i; j++)
+					free(p[j]);
+				free(p);
+				return (NULL);
+			}
 		}
+		for (a = 0; a < height; a++)
+		{
+			for (b = 0; b < width; b++)
+			{
+				p[a][b] = 0;
+			}
+		}
+		return (p);
 	}
-	return (iarray);
 }
