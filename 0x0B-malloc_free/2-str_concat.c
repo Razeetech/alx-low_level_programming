82ae72b3a97e8b89a614e5ab265078fc0170fe57
Razeetech <rajiazeezsch@gmail.com>
2023-01-10 19:50:11 -0800

    taks 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,8 @@ char *str_concat(char *s1, char *s2)
 {
 	unsigned int len1, len2, size, i, j;
 	char *nstr;
-	if (s1 ==NULL)
+
+	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
@@ -19,7 +20,7 @@ char *str_concat(char *s1, char *s2)
 	len1 = 0;
 	while (s1[len1] != '\0')
 		len1++;
-	len2 =0;
+	len2 = 0;
 	while (s2[len2] != '\0')
 		len2++;
 
