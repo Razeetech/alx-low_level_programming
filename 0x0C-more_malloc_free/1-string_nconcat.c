451c67274bc22ab687c5bd64969dae16180b2150
Razeetech <rajiazeezsch@gmail.com>
2023-01-12 04:53:38 -0800

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,6 +23,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+	}
+
 	if (n < j)
 		j = n;
 	j += i;
@@ -42,4 +46,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	co++;
 	arr[co] = '\0';
 	return (arr);
-}
+i}
