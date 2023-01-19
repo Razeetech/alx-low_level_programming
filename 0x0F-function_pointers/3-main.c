3fcb6780f147b61127b8bd271e12cf8a0f756164
Razeetech <rajiazeezsch@gmail.com>
2023-01-18 19:07:22 -0800

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,48 +2,35 @@
 #include <stdio.h>
 #include "3-calc.h"
 /**
- * main - get_op_func has operators correlated with
- * func signs and funcs from op_func
- * if not 4 arguments, return Error & exit 98
- * if op is null, return Error & exit 99
- * if div or mod 0, return Error & exit 100
- * run calc, input one, operator, input two = pointer res to get_op
+ * main - check arguments
  * @argc: arguments
  * @argv: double pointer to arguments
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int arg1, arg2, result;
-	char o;
-	int (*func)(int, int);
+	int a = 0, b = 0, res = 0;
+	char s;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
-
-	func = get_op_func(argv[2]);
-
-	if (!func)
+	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	o = *argv[2];
-
-	if ((o == '/' || o == '%') && arg2 == 0)
+	s = argv[2][0];
+	if (s != '+' && s != '-' && s != '*' && s != '%')
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
-	reult = func(arg1, arg2);
-
-	printf("%d\n", result);
-
+	a = atoi(argv[1]);
+	b = atoi9argv[3]);
+	res = (get_op_func(argv[2]))(a, b);
+	printf("%d\n", res);
 	return (0);
 }
